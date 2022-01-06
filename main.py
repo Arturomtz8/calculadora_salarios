93bf15d45a5102e9d13cd8cd4432320e30ca17a9
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-06 13:54:52 -0600

    creada una clase para manejar el tablero y modificada lógica de métodos

--- main.py
+++ main.py
@@ -9,77 +9,96 @@ niveles =  {
     'CUAUH' : 20,
 }
 
+class TableroJugadores:
+    def __init__(self, nombre_archivo):
+        self.nombre_archivo = nombre_archivo
 
-def leer_json(archivo):
-    with open(archivo) as f:
-        data = json.load(f)
-        return data
+    
+    def leer_json(self):
+        with open(self.nombre_archivo) as f:
+            data = json.load(f)
+            return data
+    
+
+    def convertir_a_dataframe(self):
+        """convierte el diccionario en un dataframe"""
+        data = self.leer_json()
+        df = pd.DataFrame(data['jugadores'])
+        return df
 
 
-def goles_por_nivel():
-    """convertir el diccionario en un dataframe y crear una nueva columna (goles_por_nivel) que correlacione A,B,C o CUAUH con el número de goles"""
-    data = leer_json('players.json')
-    df = pd.DataFrame(data['jugadores'])
+    def goles_por_nivel(self):
+        """crea una nueva columna (goles_por_nivel) que correlacione A,B,C o CUAUH de cada jugador con el número de goles que deberían meter"""
+        df = self.convertir_a_dataframe()
+        # convertir valores de columna a mayúsculas para que concuerden con diccionario "levels"
+        uppercased = df['nivel'].str.upper()
+        # crear nueva columna con los goles que debe meter el jugador dependiendo su nivel
+        df['goles_por_nivel'] = uppercased.map(niveles)
+        return df
 
-    # convertir valores de columna a mayúsculas para que concuerden con diccionario "levels"
-    uppercased = df['nivel'].str.upper()
-
-    # crear nueva columna con los goles que debe meter el jugador dependiendo su nivel
-    df['goles_por_nivel'] = uppercased.map(niveles)
-    return df
-
-    """
-    dataframe retornado
-                   nombre  nivel  goles  sueldo   bono sueldo_completo  equipo  goles_por_nivel 
+        """
+                    nombre  nivel  goles  sueldo  bono sueldo_completo  equipo  goles_por_nivel 
         0      Juan Perez      C     10   50000  25000            None   rojo               15               
         1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20             
         2  Cosme Fulanito      A      7   20000  10000            None   azul                5                   
         3         El Rulo      B      9   30000  15000            None   rojo               10 
-    """
+        """
 
 
-def goles_por_equipo():
-    """crear una nueva columna (total_goles_por_equipo) para sumar el total de goles por equipo"""
-    data = goles_por_nivel()
-    agrupado = data.groupby('equipo', as_index=False)['goles'].sum()
-    columna_suma = agrupado['goles'].astype(str) + ' ' + agrupado['equipo']
-    data['total_goles_por_equipo'] = columna_suma
-    return data
+    def total_goles_equipo(self):
+        """crea una nueva columna para sumar el total de goles por equipo"""
+        df = self.convertir_a_dataframe()
+        agrupado = df.groupby('equipo', as_index=False)['goles'].sum()
+        columna_suma = agrupado['goles'].astype(str) + ' ' + agrupado['equipo']
+        return columna_suma
+
+        """
+        0    37 azul
+        1    19 rojo
+        dtype: object
+        """
 
 
-    """
-    dataframe retornado
-                nombre  nivel  goles  sueldo  bono sueldo_completo equipo  goles_por_nivel total_goles_por_equipo
-    0      Juan Perez      C     10   50000  25000            None   rojo               15                37 azul
-    1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20                19 rojo
-    2  Cosme Fulanito      A      7   20000  10000            None   azul                5                    NaN
-    3         El Rulo      B      9   30000  15000            None   rojo               10                    NaN
-    """
+    def objetivo_goles_equipo(self):
+        """crea un data frame donde suma, basado en el nivel de cada jugador, el objetivo deseado de goles por equipo"""
+        df = self.goles_por_nivel()
+        agrupado = df.groupby('equipo', as_index=False)['goles_por_nivel'].sum()
+        columna_suma = agrupado['goles_por_nivel'].astype(str) + ' ' + agrupado['equipo']
+        df['objetivo_goles_equipo'] = columna_suma
+        return df
+
+        """
+                   nombre  nivel  goles  sueldo   bono sueldo_completo equipo  goles_por_nivel    objetivo_goles_equipo
+        0      Juan Perez      C     10   50000  25000            None   rojo               15                   25 azul
+        1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20                   25 rojo
+        2  Cosme Fulanito      A      7   20000  10000            None   azul                5                       NaN
+        3         El Rulo      B      9   30000  15000            None   rojo               10                       NaN
+        """
+    
+
+    def unir_dataframe(self):
+        """une la columna de los goles anotados realmente por el equipo con las columnas "goles_por_nivel" y "objetivo_goles_equipo" """
+        df = self.objetivo_goles_equipo()
+        df['total_goles_equipo'] = self.total_goles_equipo()
+        return df
+
+        """
+                   nombre  nivel  goles  sueldo   bono sueldo_completo equipo  goles_por_nivel objetivo_goles_equipo total_goles_equipo
+        0      Juan Perez      C     10   50000  25000            None   rojo               15               25 azul            37 azul
+        1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20               25 rojo            19 rojo
+        2  Cosme Fulanito      A      7   20000  10000            None   azul                5                   NaN                NaN
+        3         El Rulo      B      9   30000  15000            None   rojo               10                   NaN                NaN
+        """
 
 
-def objetivo_goles_por_equipo():
-    data = goles_por_equipo()
-    agrupado = data.groupby('equipo', as_index=False)['goles_por_nivel'].sum()
-    columna_suma = agrupado['goles_por_nivel'].astype(str) + ' ' + agrupado['equipo']
-    data['objetivo_goles_por_equipo'] = columna_suma
-    return data
 
-    """
-    dataframe retornado
-           nombre  nivel  goles  sueldo   bono sueldo_completo  equipo  goles_por_nivel total_goles_por_equipo meta_goles_por_equipo
-0      Juan Perez      C     10   50000  25000            None   rojo               15                37 azul               25 azul
-1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20                19 rojo               25 rojo
-2  Cosme Fulanito      A      7   20000  10000            None   azul                5                    NaN                   NaN
-3         El Rulo      B      9   30000  15000            None   rojo               10                    NaN                   NaN
-    """
-
-
-print(objetivo_goles_por_equipo())
+tablero = TableroJugadores('players.json')
+print(tablero.unir_dataframe())
 
 
 
 
-# print(data.total_goles_por_equipo.str.split(' ').str[0])
+# print(data.total_total_goles_por_equipo.str.split(' ').str[0])
 
 
 
