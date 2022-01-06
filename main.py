516ec4582a5c79941140c22f3b2a92f1b0a464d1
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-05 19:51:48 -0600

    el json se pasó a un dataframe

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import json
+import pandas as pd
 
-with open('players.json') as file:
-    data = json.load(file)
-    for player in data['jugadores']:
-        print(player)
+
+def open_json(file_name):
+    with open(file_name) as file:
+        data = json.load(file)
+        return data
+
+
+def convert_into_dataframe():
+    data = open_json("players.json")
+    df = pd.DataFrame(data['jugadores'])
+
+    return df
+     
