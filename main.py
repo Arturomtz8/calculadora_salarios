d055e116d515cbf1925cd8341f1c63aa516c17fb
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-11 23:29:34 -0600

    mejorada lógica de funciones

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
-from typing import final
-from numpy import int0
 import pandas as pd
-from functools import reduce
 
 
 niveles =  {
@@ -41,46 +38,17 @@ class TableroJugadores:
         return df
 
         """
-                    nombre  nivel  goles  sueldo  bono sueldo_completo  equipo  goles_por_nivel 
-        0      Juan Perez      C     10   50000  25000            None   rojo               15               
-        1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20             
-        2  Cosme Fulanito      A      7   20000  10000            None   azul                5                   
-        3         El Rulo      B      9   30000  15000            None   rojo               10 
-        """
-
-
-    def objetivo_goles_equipo(self):
-        """calcular el objetivo de goles por equipo dependiendo del nivel de sus jugadores"""
-        df = self.goles_por_nivel()
-        objetivo_goles_equipo = df.groupby('equipo').agg({'goles_por_nivel': 'sum'})
-        objetivo_goles_equipo.reset_index(level=0, inplace=True)
-        objetivo_goles_equipo.rename(columns={'goles_por_nivel': 'objetivo_goles_equipo'}, inplace=True)
-        return objetivo_goles_equipo
-    
-        """
-          equipo  objetivo_goles_equipo
-        0   azul                     25
-        1   rojo                     25
-        """
-
-
-    def total_goles_equipo(self):
-        """crea una nueva columna para sumar el total de goles por equipo"""
-        df = self.convertir_a_dataframe()
-        goles_equipo = df.groupby('equipo').agg({'goles': 'sum'})
-        goles_equipo.reset_index(level=0, inplace=True)
-        goles_equipo.rename(columns={'goles': 'total_goles_equipo'}, inplace=True)
-        return goles_equipo
-
-        """
-          equipo  total_goles_equipo
-        0   azul                  37
-        1   rojo                  19
+                   nombre  nivel  goles  sueldo   bono sueldo_completo equipo  goles_por_nivel
+        0      Juan Perez      C     10   50000  25000            None   rojo               15
+        1        EL Cuauh  Cuauh     30  100000  30000            None   azul               20
+        2  Cosme Fulanito      A      7   20000  10000            None   azul                5
+        3         El Rulo      B      9   30000  15000            None   rojo               10
         """
 
 
     def porcentaje_goles_equipo(self):
-        """calcula el porcentaje de goles por equipo que ayudará para deducir el 50% del bono de los jugadores"""
+        """calcula el porcentaje de goles por equipo, comparando el objetivo de goles vs los goles totales,
+         que ayudará para deducir el 50% del bono de los jugadores"""
         df = self.goles_por_nivel()
         goles_equipo = df.groupby('equipo').agg({'goles': 'sum'}) * 100
         objetivo_goles_equipo = df.groupby('equipo').agg({'goles_por_nivel': 'sum'})
@@ -97,7 +65,8 @@ class TableroJugadores:
         
 
     def porcentaje_goles_jugador(self):
-        """calcula el porcentaje de goles por jugador para deducir el otro 50% del bono de los jugadores"""
+        """deduce el otro 50% del bono de los jugadores. Compara el objetivo de goles de cada 
+        jugador dependiendo su nivel vs el total de goles individuales"""
         df = self.goles_por_nivel()
         total_goles = df.groupby('nombre').agg({'goles': 'sum'}) * 100
         objetivo_goles_nivel = df.groupby('nombre').agg({'goles_por_nivel': 'sum'})
@@ -107,56 +76,65 @@ class TableroJugadores:
         return porcentaje_goles_jugador
         
         """
-                   nombre  porcentaje_goles_jugador
-        0  Cosme Fulanito                     140.0
-        1        EL Cuauh                     150.0
-        2         El Rulo                      90.0
-        3      Juan Perez                      66.7
+                   nombre   porcentaje_goles_jugador
+        0  Cosme Fulanito                     140.00
+        1        EL Cuauh                     150.00
+        2         El Rulo                      90.00
+        3      Juan Perez                      66.67
         """
 
 
     def unir_dataframe(self):
-        df = self.goles_por_nivel()
-        df2 = self.total_goles_equipo()
-        df3 = self.objetivo_goles_equipo()
-        df4 = self.porcentaje_goles_equipo()
-        df5 = self.porcentaje_goles_jugador()
-        dfs = [df, df2, df3, df4]
-        df_con_cantidades_equipo = reduce(lambda left,right: pd.merge(left,right,on='equipo'), dfs)
-        final_df = pd.merge(df_con_cantidades_equipo, df5)
+        """une tres dataframes: 1) el primero sacado del json, 2) el dataframe que contiene el porcentaje de goles por equipo y 3) el
+        dataframe que tiene el porcentaje de goles por jugador"""
+        df_inicial = self.convertir_a_dataframe()
+        df_goles_equipo = self.porcentaje_goles_equipo()
+        df_goles_jugador= self.porcentaje_goles_jugador()
+        final_df = df_inicial.merge(df_goles_jugador,on='nombre').merge(df_goles_equipo,on='equipo')
         return final_df
 
         """
-               nombre  nivel  goles  sueldo   bono sueldo_completo equipo  goles_por_nivel  total_goles_equipo  objetivo_goles_equipo  porcentaje_goles_equipo  porcentaje_goles_jugador
-    0      Juan Perez      C     10   50000  25000            None   rojo               15                  19                     25                       76                      66.7
-    1         El Rulo      B      9   30000  15000            None   rojo               10                  19                     25                       76                      90.0
-    2        EL Cuauh  Cuauh     30  100000  30000            None   azul               20                  37                     25                      148                     150.0
-    3  Cosme Fulanito      A      7   20000  10000            None   azul                5                  37                     25                      148                     140.0
+                   nombre  nivel  goles  sueldo   bono sueldo_completo equipo  porcentaje_goles_jugador  porcentaje_goles_equipo
+        0      Juan Perez      C     10   50000  25000            None   rojo                     66.67                       76
+        1         El Rulo      B      9   30000  15000            None   rojo                     90.00                       76
+        2        EL Cuauh  Cuauh     30  100000  30000            None   azul                    150.00                      148
+        3  Cosme Fulanito      A      7   20000  10000            None   azul                    140.00                      148
+
         """
 
 
     def calcular_salario(self):
+        """primero se obtiene el porcentaje del bono que le corresponde a cada jugador dependiendo del porcentaje de goles individuales y elporcentaje 
+        de goles de su equipo. Ese porcentaje  ayuda para saber qué tanto del bono tendrá el jugador. Finalmente, el bono se suma al salario fijo"""
         final_df = self.unir_dataframe()
         porcentaje_bono = (final_df['porcentaje_goles_equipo'] + final_df['porcentaje_goles_jugador']) / 2
         bono = ((porcentaje_bono * final_df['bono']) / 100) 
         final_df['sueldo_completo'] = (final_df['sueldo'] + bono).astype(float).round(2)
         return final_df
 
+        """
+                   nombre  nivel  goles  sueldo   bono  sueldo_completo equipo   porcentaje_goles_jugador  porcentaje_goles_equipo
+        0      Juan Perez      C     10   50000  25000         67833.75   rojo                     66.67                       76
+        1         El Rulo      B      9   30000  15000         42450.00   rojo                     90.00                       76
+        2        EL Cuauh  Cuauh     30  100000  30000        144700.00   azul                    150.00                      148
+        3  Cosme Fulanito      A      7   20000  10000         34400.00   azul                    140.00                      148
+        """
+
 
     def convertir_a_json(self):
+        """guarda el dataframe en formato json. El archivo se guardará en la misma carpeta donde se ejecute el programa y su nombre será igual al nombre original
+        del archivo más 'output' para indicar que es el json con la columna del sueldo con valores"""
         dicc_jugadores = dict()
         con_salario_df = self.calcular_salario()
-        con_salario_df.drop(['goles_por_nivel', 'total_goles_equipo', 'objetivo_goles_equipo', 
-                        'porcentaje_goles_equipo', 'porcentaje_goles_jugador'], axis=1, inplace=True)
+        con_salario_df.drop(['porcentaje_goles_jugador', 'porcentaje_goles_equipo'], axis=1, inplace=True)
         dicc_jugadores['jugadores'] = con_salario_df.to_dict('records')
-        with open('prueba_resultado.json', 'w') as f:
+        with open(f'{self.nombre_archivo}output.json'.replace('.json','_', 1), 'w') as f:
             json.dump(dicc_jugadores, f, indent=4)
         return dicc_jugadores
 
 
-tablero = TableroJugadores('prueba.json')
-# print(tablero.objetivo_goles_equipo())
-# print(tablero.total_goles_equipo())
+tablero = TableroJugadores('primera_prueba.json')
+# print(tablero.goles_por_nivel())
 # print(tablero.porcentaje_goles_equipo())
 # print(tablero.porcentaje_goles_jugador())
 # print(tablero.unir_dataframe())
